a89d6c1d28eb13f3f16362fedd89e445d919a9b0
drozd <mayakovsky03@gmail.com>
2025-09-21 01:12:39 +0300

    Add feature: second task deleted

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,10 @@ export const App = () => {
         {id: 5, title: 'TypeScript', isDone: false},
         {id: 6, title: 'NodeJS', isDone: false},
     ]
-    const tasks2: Task[] = []
 
     return (
         <div className="app">
             <TodolistItem title="What to learn" tasks={tasks1} date={ '21-09-2025'}/>
-            <TodolistItem title="Songs" tasks={tasks2} />
         </div>
     )
 }
